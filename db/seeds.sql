8ee1e5a10488923e9b6f70244abc5790ab72ecc0
Rich Capuy <rccapuy@gmail.com>
2020-05-30 18:44:04 -0400

    db and table

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Big Mac", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Whooper", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("In N Out Cheeseburger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Shack Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Au Cheval Burger", true);
